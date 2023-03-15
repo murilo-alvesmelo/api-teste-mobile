026c22d311181358e138436c0d9883dc30206239
murilo-alvesmelo <murilo.melo72@gmail.com>
2023-03-15 10:40:32 -0300

    Integrando API

---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const fs = require('fs');
+
+app.use(cors())
+
+app.get('/', function(req, res) {
+  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json);
+  });
+});
+app.get('/monitoramentos', function(req, res) {
+  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json.monitoramentos);
+  });
+});
+app.get('/monitoramentos/:id', function(req, res) {
+  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json.monitoramentos[req.params.id]);
+  });
+});
+app.get('/propriedades', function(req, res) {
+  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json.propriedades);
+  });
+});
+app.get('/produtores', function(req, res) {
+  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json.produtores);
+  });
+});
+app.get('/vinculo', function(req, res) {
+  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json.vinculo);
+  });
+});
+app.get('/vinculo/:id', function(req, res) {
+  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json.vinculo[req.params.id]);
+  });
+});
+app.get('/produtores/:id', function(req, res) {
+  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json.produtores[req.params.id]);
+  });
+});
+app.get('/propriedades/:id', function(req, res) {
+  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json.propriedades[req.params.id]);
+  });
+});
+
+app.listen(3001, function() {
+  console.log('Servidor iniciado na porta 3001');
+});
