9af2a66482d130cb8d89904640cb34461349b5f7
murilo-alvesmelo <murilo.melo72@gmail.com>
2023-07-28 16:02:13 -0300

    Update API teste mobile

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3001;
 
 app.use(cors())
 
-app.get('/', function(req, res) {
-  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
+app.get('/propriedades', function (req, res) {
+  fs.readFile('./src/api/propriedades.json', function (err, data) {
     if (err) {
       throw err;
     }
@@ -15,71 +15,8 @@ app.get('/', function(req, res) {
     res.send(json);
   });
 });
-app.get('/monitoramentos', function(req, res) {
-  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
-    if (err) {
-      throw err;
-    }
-    const json = JSON.parse(data);
-    res.send(json.monitoramentos);
-  });
-});
-app.get('/monitoramentos/:id', function(req, res) {
-  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
-    if (err) {
-      throw err;
-    }
-    const json = JSON.parse(data);
-    res.send(json.monitoramentos[req.params.id]);
-  });
-});
-app.get('/propriedades', function(req, res) {
-  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
-    if (err) {
-      throw err;
-    }
-    const json = JSON.parse(data);
-    res.send(json.propriedades);
-  });
-});
-app.get('/produtores', function(req, res) {
-  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
-    if (err) {
-      throw err;
-    }
-    const json = JSON.parse(data);
-    res.send(json.produtores);
-  });
-});
-app.get('/vinculo', function(req, res) {
-  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
-    if (err) {
-      throw err;
-    }
-    const json = JSON.parse(data);
-    res.send(json.vinculo);
-  });
-});
-app.get('/vinculo/:id', function(req, res) {
-  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
-    if (err) {
-      throw err;
-    }
-    const json = JSON.parse(data);
-    res.send(json.vinculo[req.params.id]);
-  });
-});
-app.get('/produtores/:id', function(req, res) {
-  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
-    if (err) {
-      throw err;
-    }
-    const json = JSON.parse(data);
-    res.send(json.produtores[req.params.id]);
-  });
-});
-app.get('/propriedades/:id', function(req, res) {
-  fs.readFile('./src/api/processo-seletivo-front.json', function(err, data) {
+app.get('/propriedades/:id', function (req, res) {
+  fs.readFile('./src/api/propriedades.json', function (err, data) {
     if (err) {
       throw err;
     }
@@ -88,6 +25,94 @@ app.get('/propriedades/:id', function(req, res) {
   });
 });
 
-app.listen(port, function() {
+app.get('/produtores', function (req, res) {
+  fs.readFile('./src/api/produtores.json', function (err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json);
+  });
+});
+app.get('/produtores/:id', function (req, res) {
+  fs.readFile('./src/api/produtores.json', function (err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json.produtores[req.params.id]);
+  });
+});
+
+app.get('/usuarios', function (req, res) {
+  fs.readFile('./src/api/usuarios.json', function (err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json);
+  });
+});
+app.get('/usuarios/:id', function (req, res) {
+  fs.readFile('./src/api/usuarios.json', function (err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json.usuarios[req.params.id]);
+  });
+});
+
+
+app.get('/vinculos', function (req, res) {
+  fs.readFile('./src/api/vinculos.json', function (err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json);
+  });
+});
+app.get('/vinculos/:id', function (req, res) {
+  fs.readFile('./src/api/vinculos.json', function (err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    res.send(json.vinculos[req.params.id]);
+  });
+});
+app.get('/vinculosPropriedade/:id', function(req, res) {
+  fs.readFile('./src/api/vinculos.json', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    const propriedade = json.vinculos.filter(item => item.idPropriedade === parseInt(req.params.id));
+    
+    if(propriedade.length > 0) {
+      res.send(propriedade)
+    }else{
+      res.status(404).send('Nenhuma propriedade encontrada')
+    }
+  });
+});
+app.get('/findNumeroCar/:numeroCar', function (req, res) {
+  fs.readFile('./src/api/propriedades.json', function (err, data) {
+    if (err) {
+      throw err;
+    }
+    const json = JSON.parse(data);
+    const propriedade = json.propriedades.filter(item => item.numeroCar === req.params.numeroCar)
+
+    if(propriedade) {
+      res.send(propriedade)
+    }else{
+      res.status(404).send('Nenhuma propriedade foi encontrada')
+    }
+  });
+});
+
+app.listen(port, function () {
   console.log(`Servidor iniciado na porta ${port}`);
 });
